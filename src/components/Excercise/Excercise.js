fc505408ecd44796b2e46c26fed0a77ca75ef878
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-09-29 11:56:10 +0600

    excercise component added

---
+++ src/components/Excercise/Excercise.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Excercise.css';
+
+const Excercise = (props) => {
+    const { name, detail, img, rules } = props.exercise;
+    console.log(props.exercise);
+    return (
+        <div className='single-excercise-container'>
+            <img src={img} alt="" />
+            <div className='details'>
+                <h2>{name}</h2>
+                <p>{detail.slice(0, 80)}...<a href=''>more</a></p>
+            </div>
+            <div className='rules'>
+                <h3>Rules:</h3>
+                {
+                    rules.map(rule => <h6>{rule}</h6>)
+                }
+            </div>
+            <div className='ex-button-container'>
+                <button className='ex-button'>Add to Your List</button>
+            </div>
+        </div>
+    );
+};
+
+export default Excercise;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Excercise from '../Excercise/Excercise';
 import './Main.css';
 
 const Main = () => {
+    const [exercises, setExercise] = useState([]);
+    useEffect(() => {
+        fetch('fakedata.json')
+            .then(res => res.json())
+            .then(data => setExercise(data))
+    }, [])
     return (
         <div className='main-container'>
             <div className="exercise-container">
-                <h1>Our Gym Service</h1>
+                {
+                    exercises.map(exercise => <Excercise key={exercise.id} exercise={exercise}></Excercise>)
+                }
+
             </div>
             <div className="sidebar">
                 <h1>This is sidebar</h1>
