50fe739db3191bf9eab16178551222b9dc7b8d07
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-09-30 12:08:00 +0600

    question added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -37,7 +37,6 @@ const Main = () => {
                 {
                     exercises.map(exercise => <Excercise key={exercise.id} exercise={exercise} selectHandler={selectHandler}></Excercise>)
                 }
-
             </div>
             <div className="sidebar">
 
@@ -48,6 +47,25 @@ const Main = () => {
 
                 <Break selectBreakHandler={selectBreakHandler}></Break>
                 <Duration times={times} breaks={breaks}></Duration>
+            </div>
+            <div className='question-container'>
+                <h1>Some Answers of Questions About React.</h1>
+                <div>
+                    <h2>How Does React Work</h2>
+                    <h4>Ans: React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”. React has a few different kinds of components, but we'll start with React.Component subclasses: class ShoppingList extends React.</h4>
+                </div>
+                <div>
+                    <h2>what is the difference between state and props</h2>
+                    <h4>Ans: Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</h4>
+                </div>
+                <div>
+                    <h2>what are the reasons for the use effect without the reason of data load?</h2>
+                    <h4>Ans:<br></br>
+                        1.Running on state change: validating input field.<br></br>
+                        2.Running on state change: live filtering.<br></br>
+                        3.Running on state change: trigger animation on new array value.<br></br>
+                        4.Running on props change: update paragraph list on fetched API data update.</h4>
+                </div>
             </div>
         </div>
     );
